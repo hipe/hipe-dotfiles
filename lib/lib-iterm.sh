4a5cd08de0bf2b9caf167817d73c158463bb93ac
Mark Meves <mark.meves@gmail.com>
2010-07-06 04:40:17 -0400

    * add: three working functions in library file now!

--- lib/lib-iterm.sh
+++ lib/lib-iterm.sh
@@ -1,6 +1,41 @@
 #!/bin/bash
 
-function set_bg_color {
+# adapted from http://kpumuk.info/mac-os-x/how-to-show-ssh-host-name-on-the-iterms-background/
+
+function iterm_dimensions_get {
+  local size=( $(osascript -e "tell application \"iTerm\"
+		get bounds of window 1
+		end tell" | tr ',' ' ')
+	)
+  local x1=${size[0]} y1=${size[1]} x2=${size[2]} y2=${size[3]}
+  # 15px - scrollbar width
+  local w=$(( $x2 - $x1 - 15 ))
+  # 44px - titlebar + tabs height
+  local h=$(( $y2 - $y1 - 44))
+  echo "${w}x${h}"
+}
+
+
+function iterm_bg_image_set {
+	local tty=$(tty)
+	osascript -e "
+		tell application \"iTerm\"
+			repeat with theTerminal in terminals
+				tell theTerminal
+					try
+						tell session id \"$tty\"
+							set background image path to \"$1\"
+						end tell
+						on error errmesg number errn
+					end try
+				end tell
+			end repeat
+		end tell
+	"
+}
+
+
+function iterm_bg_color_set {
   local tty=$(tty)
   osascript -e "
 		tell application \"iTerm\"
@@ -15,9 +50,12 @@ function set_bg_color {
 					end try
 				end tell
 			end repeat
-		end tell"
+		end tell
+	"
 }
 
-# set_bg "/tmp/iTermBG.tmp.png"
-echo "setting to $1"
-set_bg_color $1
+echo "here are dimensions:"
+echo $(iterm_dimensions_get)
+# echo "setting bgcolor to to $1"
+# iterm_bg_color_set $1
+iterm_bg_image_set "/tmp/iTermBG.tmp.png"
