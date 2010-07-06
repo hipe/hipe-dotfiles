f94d10ee77097c49d30b9039cbbc8977e1d9c3d6
Mark Meves <mark.meves@gmail.com>
2010-07-06 04:16:09 -0400

    * change: fix this to work with bg image

--- bin/xtail-with-colors.sh
+++ bin/xtail-with-colors.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-
 function set_bg {
-	echo "running thing"
   local tty=$(tty)
   osascript -e "
-  tell application \"iTerm\"
-    repeat with theTerminal in terminals
-      tell theTerminal
-        try
-          tell session id \"$tty\"
-            set background color to \"blue\"
-          end tell
-          on error errmesg number errn
-        end try
-      end tell
-    end repeat
-  end tell"
+tell application \"iTerm\"
+repeat with theTerminal in terminals
+tell theTerminal
+try
+tell session id \"$tty\"
+set background image path to \"$1\"
+end tell
+on error errmesg number errn
+end try
+end tell
+end repeat
+end tell"
 }
-echo "running..."
-set_bg
+
+set_bg "/tmp/iTermBG.tmp.png"
