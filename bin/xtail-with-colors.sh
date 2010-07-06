78cd3e2d6c347ecfddc11861be97deea4fa94258
Mark Meves <mark.meves@gmail.com>
2010-07-06 04:17:16 -0400

    * change: fixed!

--- bin/xtail-with-colors.sh
+++ bin/xtail-with-colors.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-function set_bg {
+function set_bg_color {
   local tty=$(tty)
   osascript -e "
-tell application \"iTerm\"
-repeat with theTerminal in terminals
-tell theTerminal
-try
-tell session id \"$tty\"
-set background image path to \"$1\"
-end tell
-on error errmesg number errn
-end try
-end tell
-end repeat
-end tell"
+		tell application \"iTerm\"
+			repeat with theTerminal in terminals
+				tell theTerminal
+					try
+						tell session id \"$tty\"
+							set background image path to \"\"
+							set background color to \"$1\"
+						end tell
+						on error errmesg number errn
+					end try
+				end tell
+			end repeat
+		end tell"
 }
 
-set_bg "/tmp/iTermBG.tmp.png"
+# set_bg "/tmp/iTermBG.tmp.png"
+echo "setting to $1"
+set_bg_color $1
