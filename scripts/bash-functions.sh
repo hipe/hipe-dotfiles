9afa0c8dfc4c767a224b19b6dcdc46bcc0082b4e
Mark Meves (hipe) <mark.meves@gmail.com>
2010-07-12 06:04:53 +0000

    * moved: this to own file

---
+++ scripts/bash-functions.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Ridiculous functions
+# I got the following from, and mod'd it: http://www.macosxhints.com/article.php?story=20020716005123797
+#    The following aliases (save & show) are for saving frequently used directories
+#    You can save a directory using an abbreviation of your choosing. Eg. save ms
+#    You can subsequently move to one of the saved directories by using cd with
+#    the abbreviation you chose. Eg. cd ms  (Note that no '$' is necessary.)
+if [ ! -f ~/.dirs ]; then
+  touch ~/.dirs
+fi
+
+alias show='cat ~/.dirs'
+save (){
+ command sed "/!$/d" ~/.dirs > ~/.dirs1; \mv ~/.dirs1 ~/.dirs; echo "$@"=\"`pwd`\" >> ~/.dirs; source ~/.dirs ;
+}
+source ~/.dirs  # Initialization for the above 'save' facility: source the .sdirs file
+shopt -s cdable_vars # set the bash option so that no '$' is required when using the above facility
+
