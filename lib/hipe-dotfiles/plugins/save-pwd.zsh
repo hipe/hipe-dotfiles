e0df731efd90d9906760eaa1f49332c5719ffe4a
Mark Meves <mark.meves@gmail.com>
2012-05-23 12:22:02 -0400

    [#002] (for posterity here is the super-short version we had on the mb air, not versioned)

--- lib/hipe-dotfiles/plugins/save-pwd.zsh
+++ lib/hipe-dotfiles/plugins/save-pwd.zsh
@@ -1,20 +1,7 @@
 #!/bin/zsh
-
-# I got the following from, and mod'd it: http://www.macosxhints.com/article.php?story=20020716005123797
-#    The following aliases (save & show) are for saving frequently used directories
-#    You can save a directory using an abbreviation of your choosing. Eg. save ms
-#    You can subsequently move to one of the saved directories by using cd with
-#    the abbreviation you chose. Eg. cd ms  (Note that no '$' is necessary.)
-
-if [ ! -f ~/.dirs ]; then
-  touch ~/.dirs
-fi
-
-save () {
-  command sed "/!$/d" ~/.dirs | sort > ~/.dirs1
-  \mv ~/.dirs1 ~/.dirs
-  echo "$@"=\"`pwd`\" >> ~/.dirs
-  source ~/.dirs
+[ ! -f ~/.dirs ] && touch ~/.dirs
+save (){
+ command sed "/!$/d" ~/.dirs > ~/.dirs1; \mv ~/.dirs1 ~/.dirs; echo "$@"=\"`pwd`\" >> ~/.dirs; source ~/.dirs ;
 }
-source ~/.dirs  # Initialization for the above 'save' facility: source the .sdirs file
-
+source ~/.dirs
+setopt cdablevars
