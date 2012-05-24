ed28ff8af5cdf97b01d57f977f84d9e4c0610a29
Mark Meves <mark.meves@gmail.com>
2012-05-24 00:25:49 -0400

    [#002] add some new little scriptlettes that we had on mb air, experiment w/ new architecture

--- doc/issues.md
+++ doc/issues.md
@@ -1,2 +1,3 @@
+#003 2012-05-24 @open remove/process all _junk
 #002 2012-05-23 @open sweet-ass loading of some little plugins (sub plugins!)
 #001 2012-05-23 @closed remove some annoying vim crap
---
+++ lib/hipe-dotfiles/plugins/available/git.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+export PATH="$PATH:/usr/local/git/bin"
+
+function gitignore {
+  local file='.gitignore'
+  if [[ ! -f $file ]] ; then
+    echo "not found: $file"
+    return 0
+  fi
+  cat $file | sort > tmp.tmp
+  mv tmp.tmp .gitignore
+  echo "resorted $file."
+}
